39cdad1c302e20d35c872657e89e33ca95953165
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-09 13:29:31 +0530

    Create 2cprogramm9-2.c

---
+++ 2cprogramm9-2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+
+printf("enter the number to find armstromg");
+scanf("%d",&onum);
+num=onum;
+while(num<=100000)
+{
+while(num!=0)
+{
+rem=num%10;
+result+=rem*rem*rem;
+num=num/10;
+}
+}
+if(num==onum)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
