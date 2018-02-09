33a605d4d4be7073808626b0d4900d08d25afc6e
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-09 13:27:15 +0530

    Create 1cprogramm9-2.c

---
+++ 1cprogramm9-2.c
@@ -0,0 +1,19 @@
+a=int(input())
+b=int(input())
+for n in range(a+2,b):
+	f=0
+	for i in range(2,n//2):
+			if n%i==0:
+				f=1
+				break
+	if f==0:
+		print(n)a=int(input())
+b=int(input())
+for n in range(a+2,b):
+	f=0
+	for i in range(2,n//2):
+			if n%i==0:
+				f=1
+				break
+	if f==0:
+		print(n)
